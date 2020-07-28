dbef18bff6c15fcb5c47e98007a8bd65b51c1de2
luk-str <lstrociak@gmail.com>
2020-07-28 09:55:12 +0200

    add production build

---
+++ build/precache-manifest.539e43e76191eb717f5f8ac01ba63740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c8c65b8444bcddf785e7dc90fbe414",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3644ff024ad8c3e5c40a",
+    "url": "/static/css/main.d4cf92e5.chunk.css"
+  },
+  {
+    "revision": "fa9ce8cd690013f4bcb9",
+    "url": "/static/js/2.521aaab3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.521aaab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3644ff024ad8c3e5c40a",
+    "url": "/static/js/main.259e5fb3.chunk.js"
+  },
+  {
+    "revision": "31de383a84f0b531942d",
+    "url": "/static/js/runtime-main.408e7f6b.js"
+  }
+]);
